99ad292e0651fdba17723e4efc41cb0b53416df9
Senada Kadric <skadric@fordham.edu>
2020-04-29 20:04:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ def get_english_meaning(file_path, emoticon)
 
       if emojis_hashes[:japanese] == emoticon
         return key
-      elsif emojis_hashes[:japanese] != emoticon
+      else
         return "Sorry, we don't know that emoticon"
       end
 
